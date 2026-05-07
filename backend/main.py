54a1dce471ae2d13cf4d344b020d50f53d3876e1
ldotmithu <ldotmithurshan222@gmail.com>
2026-05-07 16:38:37 +0530

    some modefication on api

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
 
-# Load models
+
+app = FastAPI(
+    title="Personality Prediction API",
+    description="Predict Introvert vs Extrovert"
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 model = joblib.load("model/personality_model.pkl")
 scaler = joblib.load("model/scaler.pkl")
 
@@ -11,27 +27,31 @@ scaler = joblib.load("model/scaler.pkl")
 stage_fear_mapping = {"No": 0, "Yes": 1}
 drained_mapping = {"No": 0, "Yes": 1}
 
+
 class PersonalData(BaseModel):
     Time_spent_Alone: float
-    Stage_fear: str 
+    Stage_fear: str
     Social_event_attendance: float
     Going_outside: float
-    Drained_after_socializing: str 
+    Drained_after_socializing: str
     Friends_circle_size: float
     Post_frequency: float
 
-app = FastAPI(title="Personality Prediction API", 
-              description="Predict if a person is Extrovert or Introvert")
 
 @app.get("/")
 def home():
-    return {"status": "Application Running", "message": "Use POST /predict/ to make predictions"}
+    return {
+        "status": "API Running",
+        "message": "Use /predict endpoint for predictions"
+    }
+
 
 @app.post("/predict/")
 def predict(personal_data: PersonalData):
+
     stage_fear_encoded = stage_fear_mapping.get(personal_data.Stage_fear, 0)
     drained_encoded = drained_mapping.get(personal_data.Drained_after_socializing, 0)
-    
+
     features = np.array([[
         personal_data.Time_spent_Alone,
         stage_fear_encoded,
@@ -41,22 +61,22 @@ def predict(personal_data: PersonalData):
         personal_data.Friends_circle_size,
         personal_data.Post_frequency
     ]])
-   
+
+    
     features_scaled = scaler.transform(features)
-    
-    
+
+   
     prediction = model.predict(features_scaled)[0]
     probability = model.predict_proba(features_scaled)[0]
+
     
-   
-    personality = "Extrovert" if prediction == 0 else "Introvert"
-    
+    personality = "Extrovert" if prediction == 1 else "Introvert"
+
     return {
         "personality": personality,
         "confidence": float(max(probability)),
         "probabilities": {
-            "Extrovert": float(probability[0]),
-            "Introvert": float(probability[1])
-        },
-        "input_data": personal_data.dict()
+            "Class_0": float(probability[0]),
+            "Class_1": float(probability[1])
+        }
     }
